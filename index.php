533383f3374852d5f718cced755c876d49d3381d
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-18 14:41:50 +0700

    After Fixing With Client

--- index.php
+++ index.php
@@ -184,19 +184,6 @@
             <!-- /.info-box -->
           </div>
           <!-- /.col -->
-          <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Bingung</span>
-                <span class="info-box-number">41,410</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
           <?php } ?>
 
           <!-- fix for small devices only -->
