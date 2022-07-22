dd3114a084dc51bd091f523d68cf53146d4ff8f4
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-07-22 23:37:02 +0700

    Update vehicles.php

--- vehicles.php
+++ vehicles.php
@@ -452,7 +452,7 @@
             <div class="modal-dialog modal-lg">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h4 class="modal-title">Edit Kendaraan</h4>
+                  <h4 class="modal-title">Verifikasi KIR Kendaraan</h4>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
